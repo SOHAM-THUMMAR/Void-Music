5499a586606bde10e7bfbac38b97121d119b20fe
SOHAM-THUMMAR <sohamthummar04@gmail.com>
2025-09-19 22:55:34 +0530

    this was our php websire which we made in 4th semester of our engineering this website was really fun to make and we all enjoied it

--- about-us.php
+++ about-us.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - Fierce Femmes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
+    <style> 
         /* General Styles */
         body { 
             background-color: #1C1C1C; 
